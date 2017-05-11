7041432cd0d0458030c07b820031f56de067c004
Waqas Yousaf <wiqi@live.com>
2017-05-11 23:31:41 +0500

    bug fixes

--- wishdd-shortcodes.php
+++ wishdd-shortcodes.php
@@ -10,18 +10,18 @@
 function wishdd_gdoc_embed($atts, $content = null)
 { 
 	$atts = shortcode_atts(['id' => '0B7Ls2KNfpdLdVG1uSk9hVXJ2eTQ', 'btn_txt' => 'Download'], $atts);
-	
-	global  $sc_title, $sc_placeholer;
-	
 	$doc_id  = esc_html__($atts['id']);
-	$btn_txt =   esc_html__($atts['btn_txt']);
+	$btn_txt = esc_html__($atts['btn_txt']);
 	$btn_txt = ($btn_txt == "") ? "Downlod" :  esc_html__($atts['btn_txt']);
 	
+	$width = 640;
+	$height = ($width/8 ) * 6; 
+	
 	$output = '
-	<iframe src="https://drive.google.com/file/d/'.$doc_id.'/preview" width="640" height="480"></iframe>
+	<iframe src="https://drive.google.com/file/d/'.$doc_id.'/preview" width="'.$width.'" height="'.$height.'"></iframe>
 
 &nbsp;
-<p style="text-align: center;"><a class="btn btn-default btn-lg" href="https://drive.google.com/file/d/'.$doc_id.'/view?usp=sharing">'.$btn_txt.'</a></p>';
+<p style="width:'.$height.'; text-align: center; "><a class="btn btn-default btn-lg" href="https://drive.google.com/uc?export=download&id='.$doc_id.'">'.$btn_txt.'</a></p>';
 	return $output;
 }
 
